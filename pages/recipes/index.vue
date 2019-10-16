63bce5ae37da7633db781f90ed18165a68f42513
Pablo Cattaneo <pablocattaneo1@gmail.com>
2019-10-16 08:52:45 -0300

    #master add first card to /recipes

--- pages/recipes/index.vue
+++ pages/recipes/index.vue
@@ -1,5 +1,23 @@
 <template>
-  <h1>recipes</h1>
+  <div id="recipes">
+    <b-card
+      v-for="(recipe, index) in recipes"
+      :key="index"
+      :title="recipe.title"
+      :img-src="recipe.thumbnailImg"
+      img-alt="Image"
+      img-top
+      tag="article"
+      style="max-width: 20rem;"
+      class="mb-2"
+    >
+      <b-card-text>
+        {{ recipe.description }}
+      </b-card-text>
+
+      <b-button href="#" variant="primary">Buy ingredients</b-button>
+    </b-card>
+  </div>
 </template>
 
 <script>
