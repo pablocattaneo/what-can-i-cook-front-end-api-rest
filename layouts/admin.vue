cef3657551382b7238a073ef054d72579ff058ea
Pablo Cattaneo <pablocattaneo1@gmail.com>
2019-10-25 09:06:01 -0300

    #master create admin page and admin layout

---
+++ layouts/admin.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <nuxt-link :to="switchLocalePath('en')">English</nuxt-link>
+    <nuxt-link :to="switchLocalePath('es')">Español</nuxt-link>
+    <nuxt />
+  </div>
+</template>
---
+++ pages/admin/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>pages/admin/index.vue</h1>
+</template>
+
+<script>
+export default {
+  layout: 'admin'
+}
+</script>
+
+<style></style>
