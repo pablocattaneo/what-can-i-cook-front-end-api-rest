5cf9fc34b355d23fba3965aa7b58344f9cb804c3
Pablo Cattaneo <pablocattaneo1@gmail.com>
2019-10-26 10:57:03 -0300

    #master Was created admin/recipe view and part of a form

---
+++ pages/admin/recipes/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="admin-recipes">
+    <b-form @submit.prevent="submit" @reset="resetForm">
+      <b-form-group
+        id="recipe-name"
+        label="Recipe name:"
+        label-for="recipe-name"
+      >
+        <b-form-input
+          id="recipe-name"
+          v-model="form.recipeName"
+          required
+          placeholder="Recipe name"
+        ></b-form-input>
+      </b-form-group>
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        recipeName: ''
+      }
+    }
+  },
+  methods: {
+    submit() {
+      alert(JSON.stringify(this.form))
+    },
+    resetForm() {
+      console.log('reset')
+    }
+  }
+}
+</script>
+
+<style></style>
