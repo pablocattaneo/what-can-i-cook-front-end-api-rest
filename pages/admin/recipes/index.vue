fe78b75c27a0d5ac0378560e9eed97e929ce1bc1
Pablo Cattaneo <pablocattaneo1@gmail.com>
2019-11-03 10:54:12 -0300

    #master create first version of WcForm.vue

--- pages/admin/recipes/index.vue
+++ pages/admin/recipes/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="admin-recipes">
-    <b-form novalidate @submit.prevent="submit" @reset="resetForm">
+    <WcForm>
       <WcInput
         id="recipe-name"
-        v-model="recipeForm.fields.recipeName"
+        :v-model="recipeForm.fields.recipeName"
         :validation="$v.recipeForm.fields.recipeName"
         label="Recipe name:"
         label-for="recipe-name"
@@ -11,27 +11,21 @@
         :state="$v.recipeForm.fields.recipeName.$error ? false : null"
         autocomplete="off"
       />
-      <!-- <b-form-input list="my-list-id"></b-form-input>
-      <datalist id="my-list-id">
-        <option>Ingredients</option>
-        <option
-          v-for="(ingredient, index) in ingredients"
-          :key="`ingredients-${index}`"
-          >{{ ingredient }}</option
-        >
-      </datalist> -->
-      <b-button type="submit" variant="primary">Submit</b-button>
-      <b-button type="reset" variant="danger">Reset</b-button>
-    </b-form>
+      <b-button type="submit" variant="primary" @click="submit"
+        >Submit</b-button
+      >
+    </WcForm>
   </div>
 </template>
 
 <script>
 import { required } from 'vuelidate/lib/validators'
 import WcInput from '@/components/forms/WcInput'
+import WcForm from '@/components/forms/WcForm'
 export default {
   components: {
-    WcInput
+    WcInput,
+    WcForm
   },
   data() {
     return {
@@ -45,16 +39,16 @@ export default {
   },
   methods: {
     submit() {
-      this.recipeForm.wasFormValidated = true
-      this.$v.recipeForm.$touch()
-      if (!this.$v.recipeForm.$pending && !this.$v.recipeForm.$error) {
-        alert(JSON.stringify(this.recipeForm.fields))
-      } else {
-        alert('Error in validation')
-      }
-    },
-    resetForm() {
-      alert('reset')
+      console.log('submit')
+      this.$children[0].validationForm(this.$v).then((r) => {
+        console.log('r', r)
+      })
+      // this.$v.recipeForm.$touch()
+      // if (!this.$v.recipeForm.$pending && !this.$v.recipeForm.$error) {
+      //   alert(JSON.stringify(this.recipeForm.fields))
+      // } else {
+      //   alert('Error in validation')
+      // }
     }
   },
   validations: {
