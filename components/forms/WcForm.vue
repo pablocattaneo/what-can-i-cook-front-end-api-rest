8c0bef0d51e602630728b3032a7372ee184b1d0e
Pablo Cattaneo <pablocattaneo1@gmail.com>
2019-11-03 10:54:55 -0300

    #master create first version of WcForm.vue

---
+++ components/forms/WcForm.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-form novalidate @submit.prevent>
+    <slot></slot>
+  </b-form>
+</template>
+
+<script>
+export default {
+  methods: {
+    validationForm(validation) {
+      validation.$touch()
+      console.log('validateForm')
+      if (validation.$error) {
+        // this.showToastFormError()
+        // this.errorScrollTo()
+        return Promise.reject(new Error('validation error'))
+      } else if (!validation.$pending || !validation.$error) {
+        return Promise.resolve(true)
+      } else {
+        Promise.reject(new Error('validation error'))
+      }
+    }
+  }
+}
+</script>
