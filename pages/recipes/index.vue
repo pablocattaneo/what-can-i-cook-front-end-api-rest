a1b1b98f33ca9979d3ac2e66f03e283b54241bd3
Pablo Cattaneo <pablocattaneo1@gmail.com>
2019-11-21 09:18:12 -0300

    #master components/recipes/RecipeCard.vue component was created to reuse the code

--- pages/recipes/index.vue
+++ pages/recipes/index.vue
@@ -1,56 +1,25 @@
 <template>
   <div id="recipes">
     <RecipeFilters />
-    <b-card
+    <RecipeCard
       v-for="(recipe, recipesIndex) in recipes"
       :key="recipesIndex"
       :title="recipe.title"
-      img-alt="Image"
-      img-top
-      tag="article"
-      style="max-width: 20rem;"
-      class="mb-2"
-      border-variant="primary"
-    >
-      <b-img-lazy
-        :src="$store.state.apiRestBaseUrl + recipe.mainImg"
-        width="100%"
-        :alt="recipe.title"
-        :title="recipe.title"
-        blank-src="null"
-        class="mb-3"
-      ></b-img-lazy>
-      <b-card-text>
-        {{ recipe.description }}
-      </b-card-text>
-      <b-list-group flush>
-        <b-list-group-item
-          v-for="(info, key, index) in recipe.more_info"
-          :key="index + key"
-          >{{ $t(`recipes.${key}`) }}: {{ info }}</b-list-group-item
-        >
-      </b-list-group>
-      <b-list-group flush>
-        <b-list-group-item
-          v-for="(direction, index) in recipe.directions"
-          :key="`direction-${index}`"
-          >{{ index + 1 }} {{ direction }}</b-list-group-item
-        >
-      </b-list-group>
-      <template v-slot:footer>
-        <b-button href="#" variant="primary">{{
-          $t('recipes.buy_ingredients')
-        }}</b-button>
-      </template>
-    </b-card>
+      :b-img-lazy-src="$store.state.apiRestBaseUrl + recipe.mainImg"
+      :description="recipe.description"
+      :moreInfo="recipe.more_info"
+      :directions="recipe.directions"
+    />
   </div>
 </template>
 
 <script>
 import RecipeFilters from '@/components/recipes/RecipeFilters.vue'
+import RecipeCard from '@/components/recipes/RecipeCard.vue'
 export default {
   components: {
-    RecipeFilters
+    RecipeFilters,
+    RecipeCard
   },
   async asyncData(context) {
     const currentLang = context.app.i18n.locale
@@ -65,8 +34,3 @@ export default {
   }
 }
 </script>
-
-<style lang="stylus" scoped>
-img
-  width 100%
-</style>
