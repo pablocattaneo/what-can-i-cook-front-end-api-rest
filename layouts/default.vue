12fe45398cfa67eac7236d7765952d6a55c0c116
Pablo Cattaneo <pablocattaneo1@gmail.com>
2019-12-11 08:46:01 -0300

    #master normalize nuxt-link localePath arguments

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <nuxt-link :to="localePath('about')">{{ $t('links.about') }}</nuxt-link>
-    <nuxt-link :to="localePath('recipes')">{{ $t('links.recipes') }}</nuxt-link>
+    <nuxt-link :to="localePath({ path: '/about' })">{{
+      $t('links.about')
+    }}</nuxt-link>
+    <nuxt-link :to="localePath({ path: '/recipes' })">{{
+      $t('links.recipes')
+    }}</nuxt-link>
     <nuxt-link :to="switchLocalePath('en')">English</nuxt-link>
     <nuxt-link :to="switchLocalePath('es')">Español</nuxt-link>
     <nuxt />
