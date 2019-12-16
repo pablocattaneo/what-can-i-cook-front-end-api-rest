81dad4eb5717dc211fb6240aad4c9734c8f08af1
Pablo Cattaneo <pablocattaneo1@gmail.com>
2019-12-16 07:56:38 -0300

    #master add login page

---
+++ pages/login/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1>Login</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
