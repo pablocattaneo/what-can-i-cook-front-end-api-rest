11d89376e71d075825163c881f49ebe6983af826
Pablo Cattaneo <pablocattaneo1@gmail.com>
2019-12-18 07:48:43 -0300

    #master create new properties for WcButtonSubmit

---
+++ components/forms/WcButtonSubmit.vue
@@ -0,0 +1,38 @@
+<template>
+  <b-button
+    :disabled="disabled || isProcessing"
+    type="submit"
+    variant="primary"
+  >
+    <b-spinner v-if="isProcessing" small type="grow" />
+    <template v-if="isProcessing">
+      {{ processingTextValue || $t('form.processing') }}
+    </template>
+    <template v-if="!isProcessing">
+      {{ buttonTextValue || $t('form.submit') }}
+    </template>
+  </b-button>
+</template>
+
+<script>
+export default {
+  props: {
+    isProcessing: {
+      type: Boolean,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    buttonTextValue: {
+      type: String,
+      default: ''
+    },
+    processingTextValue: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
