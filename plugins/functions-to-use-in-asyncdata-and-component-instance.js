1eb8b96db02c0a223b9ab50dda27dd34db3252e0
Pablo Cattaneo <pablocattaneo1@gmail.com>
2019-12-28 11:59:20 -0300

    #master harcoded auth

--- plugins/functions-to-use-in-asyncdata-and-component-instance.js
+++ plugins/functions-to-use-in-asyncdata-and-component-instance.js
@@ -2,6 +2,11 @@ export default ({ app, route }, inject) => {
   inject('getRecipes', () => {
     const currentLang = app.i18n.locale
     const urlLangSantized = route.fullPath.replace(`/${currentLang}`, '')
-    return app.$axios.$get(urlLangSantized)
+    return app.$axios.$get(urlLangSantized, {
+      headers: {
+        Authorization:
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImplbGVuYUBtYWlsaW5hdG9yLm5ldCIsInVzZXJJZCI6IjVkZmUyZDdlYmNlYTE3MGNjNzAyZGZkZiIsImlhdCI6MTU3NzAxOTgxNCwiZXhwIjoxNTc3MDIzNDE0fQ.SMDNJh5Wcu-sovQIlwScnjmAIHi65mdLAbiehL17Au4'
+      }
+    })
   })
 }
