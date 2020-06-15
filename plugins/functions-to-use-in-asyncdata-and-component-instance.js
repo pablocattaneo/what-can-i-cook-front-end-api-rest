9f11424aaf9d35cef1c44401a665e36cc5db2f9f
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-06-15 15:55:17 -0300

    #master get all recipes

--- plugins/functions-to-use-in-asyncdata-and-component-instance.js
+++ plugins/functions-to-use-in-asyncdata-and-component-instance.js
@@ -2,10 +2,11 @@ export default ({ app, route }, inject) => {
   inject('getRecipes', () => {
     const currentLang = app.i18n.locale
     const urlLangSantized = route.fullPath.replace(`/${currentLang}`, '')
-    return app.$axios.$get(urlLangSantized, {
+    console.log('urlLangSantized', urlLangSantized)
+    return app.$axios.$get('recipes/', {
       headers: {
         Authorization:
-          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImplbGVuYUBtYWlsaW5hdG9yLm5ldCIsInVzZXJJZCI6IjVkZmUyZDdlYmNlYTE3MGNjNzAyZGZkZiIsImlhdCI6MTU3NzAxOTgxNCwiZXhwIjoxNTc3MDIzNDE0fQ.SMDNJh5Wcu-sovQIlwScnjmAIHi65mdLAbiehL17Au4'
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InBjYXR0YW5lb0BnbWFpbC5jb20iLCJ1c2VySWQiOiI1ZTQyNzZkZTdhM2M5YjI1YmM4NzM5M2IiLCJpYXQiOjE1ODE0MTQ1NzksImV4cCI6MTU4MTQxODE3OX0.16wf7CiWm43DXz8CE6Mi92l7CIN-PW1mGyL-vvIEsjQ'
       }
     })
   })
