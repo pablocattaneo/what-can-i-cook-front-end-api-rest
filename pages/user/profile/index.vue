3df63ba5882b5b583f8e7603ec25119dd1192804
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-06-24 07:52:16 -0300

    #master get user info

--- pages/user/profile/index.vue
+++ pages/user/profile/index.vue
@@ -3,7 +3,15 @@
 </template>
 
 <script>
-export default {}
+export default {
+  async asyncData({ app, store }) {
+    await app.$axios.$get(`user/${store.state.user.userId}`, {
+      headers: {
+        Authorization: 'Bearer ' + localStorage.getItem('token')
+      }
+    })
+  }
+}
 </script>
 
 <style></style>
