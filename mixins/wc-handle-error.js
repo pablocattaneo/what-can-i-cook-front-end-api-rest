0830cb5e1b0add003a05224f23d1ffd555301774
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-06-25 07:52:28 -0300

    #master improve code

--- mixins/wc-handle-error.js
+++ mixins/wc-handle-error.js
@@ -1,37 +1,48 @@
 export default {
   methods: {
-    _errorToast(
-      message,
-      body = {
-        title: 'Error',
-        autoHideDelay: 15000,
-        toaster: 'b-toaster-top-center',
-        variant: 'danger',
-        solid: true
-      }
-    ) {
-      this.$bvToast.toast(message, body)
-    },
     serverErrorsHandler(error) {
+      const errorToastParameter = {
+        message: '',
+        body: {
+          title: 'Error',
+          autoHideDelay: 15000,
+          toaster: 'b-toaster-top-center',
+          variant: 'danger',
+          solid: true
+        }
+      }
       if (error.response) {
         // The request was made and the server responded with a status code
         // that falls out of the range of 2xx
         if (error.response.data.isArray) {
           error.response.data.errors.forEach((element) => {
-            this._errorToast(element.msg)
+            errorToastParameter.message = element.msg
+            this.$bvToast.toast(
+              errorToastParameter.message,
+              errorToastParameter.body
+            )
           })
         } else {
-          console.log('21')
-          this._errorToast(error.response.data)
+          errorToastParameter.message = error.response.data
+          this.$bvToast.toast(
+            errorToastParameter.message,
+            errorToastParameter.body
+          )
         }
       } else if (error.request) {
         // The request was made but no response was received
         // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
         // http.ClientRequest in node.js
-        this._errorToast(this.$t('errors.server_is_down'))
+        this.$bvToast.toast(
+          errorToastParameter.message,
+          errorToastParameter.body
+        )
       } else {
         // Something happened in setting up the request that triggered an Error
-        this._errorToast(error.message)
+        this.$bvToast.toast(
+          errorToastParameter.message,
+          errorToastParameter.body
+        )
       }
     }
   }
