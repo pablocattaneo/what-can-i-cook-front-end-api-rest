672835e8d69a4be20d618e7b458c5410b9333a30
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-06-25 07:41:37 -0300

    #master remove redundancy in the code

--- mixins/wc-handle-error.js
+++ mixins/wc-handle-error.js
@@ -1,54 +1,37 @@
 export default {
   methods: {
+    _errorToast(
+      message,
+      body = {
+        title: 'Error',
+        autoHideDelay: 15000,
+        toaster: 'b-toaster-top-center',
+        variant: 'danger',
+        solid: true
+      }
+    ) {
+      this.$bvToast.toast(message, body)
+    },
     serverErrorsHandler(error) {
       if (error.response) {
         // The request was made and the server responded with a status code
         // that falls out of the range of 2xx
-        console.log('error.response.data', error.response.data)
-        console.log('error.response.status', error.response.status)
-        console.log('error.response.headers', error.response.headers)
         if (error.response.data.isArray) {
           error.response.data.errors.forEach((element) => {
-            this.$bvToast.toast(element.msg, {
-              title: 'Error',
-              autoHideDelay: 15000,
-              toaster: 'b-toaster-top-center',
-              variant: 'danger',
-              solid: true
-            })
+            this._errorToast(element.msg)
           })
         } else {
           console.log('21')
-          this.$bvToast.toast(error.response.data, {
-            title: 'Error',
-            autoHideDelay: 15000,
-            toaster: 'b-toaster-top-center',
-            variant: 'danger',
-            solid: true
-          })
+          this._errorToast(error.response.data)
         }
       } else if (error.request) {
         // The request was made but no response was received
         // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
         // http.ClientRequest in node.js
-        console.log('error.request', error.request)
-        this.$bvToast.toast(this.$t('errors.server_is_down'), {
-          title: 'Error',
-          autoHideDelay: 15000,
-          toaster: 'b-toaster-top-center',
-          variant: 'danger',
-          solid: true
-        })
+        this._errorToast(this.$t('errors.server_is_down'))
       } else {
         // Something happened in setting up the request that triggered an Error
-        console.log('Error', error.message)
-        this.$bvToast.toast(error.message, {
-          title: 'Error',
-          autoHideDelay: 15000,
-          toaster: 'b-toaster-top-center',
-          variant: 'danger',
-          solid: true
-        })
+        this._errorToast(error.message)
       }
     }
   }
