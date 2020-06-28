1c83e5225073bfe7579753c9fb51741421c00a20
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-06-28 17:37:07 -0300

    #mater refactoring component name

---
+++ components/forms/WcEditInlineField.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="row">
+    <p class="mt-4 mb-0">
+      <span class="font-weight-bold">{{ labels }}:</span>
+      <span>{{ actualValue }}</span>
+      <b-icon-pencil-square
+        @click="edit"
+        :animation="animation"
+        variant="primary"
+        class="ml-2 icon wc-cursor-pointer"
+        scale="1.2"
+      />
+    </p>
+    <b-collapse :id="id" class="col-12 mt-2">
+      <WcForm ref="wcForm" class="px-2">
+        <WcInput
+          id="user-name"
+          :value="wcInputValue"
+          :validation="validation"
+          :state="validation.$error ? false : null"
+          :label="labels + ':'"
+          :placeholder="labels"
+          @input="value = $event"
+          type="text"
+          class="w-100"
+        />
+        <b-button @click="closeEdit" variant="secondary">{{
+          $t('Cancel')
+        }}</b-button>
+        <WcButtonSubmit
+          @click.native="submit"
+          :isProcessing="isFormProcessing"
+        />
+      </WcForm>
+    </b-collapse>
+  </div>
+</template>
+
+<script>
+import { BIconPencilSquare } from 'bootstrap-vue'
+import WcInput from '@/components/forms/WcInput.vue'
+import WcForm from '@/components/forms/WcForm.vue'
+import WcButtonSubmit from '@/components/forms/WcButtonSubmit'
+import wcHandleError from '@/mixins/wc-handle-error.js'
+
+export default {
+  components: {
+    WcInput,
+    WcForm,
+    WcButtonSubmit,
+    BIconPencilSquare
+  },
+  mixins: [wcHandleError],
+  props: {
+    labels: {
+      type: String,
+      default: ''
+    },
+    id: {
+      type: String,
+      default: 'user-name-collapse',
+      required: true
+    },
+    objectToSendServer: {
+      type: Object,
+      default: () => ({})
+    },
+    actualValue: {
+      type: String,
+      default: ''
+    },
+    wcInputValue: {
+      type: String,
+      default: ''
+    },
+    validation: {
+      type: Object,
+      default: () => ({})
+    },
+    endPointPath: {
+      type: String,
+      default: '',
+      required: true
+    }
+  },
+  data() {
+    return {
+      isFormProcessing: false,
+      animation: null,
+      value: ''
+    }
+  },
+  methods: {
+    closeEdit() {
+      this.$root.$emit('bv::toggle::collapse', this.id)
+    },
+    edit(field) {
+      this.$root.$emit('bv::toggle::collapse', this.id)
+      this.animation = 'throb'
+      setTimeout(() => {
+        this.animation = null
+      }, 800)
+    },
+    async submit() {
+      try {
+        this.isFormProcessing = true
+        await this.$refs.wcForm.validationForm(this.validation)
+        for (const key in this.objectToSendServer.contentToUpdate) {
+          this.objectToSendServer.contentToUpdate[key] = this.value
+        }
+        await this.$axios.$post(this.endPointPath, this.objectToSendServer, {
+          headers: {
+            Authorization: 'Bearer ' + localStorage.getItem('jwtToken')
+          }
+        })
+      } catch (error) {
+        this.serverErrorsHandler(error)
+      } finally {
+        this.isFormProcessing = false
+      }
+    }
+  }
+}
+</script>
+
+<style></style>
