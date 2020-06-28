de8699a6f0c48a361aaf8ce650db9cc39d391b81
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-06-28 13:43:33 -0300

    #master create component WcEditOneField

--- pages/user/profile/index.vue
+++ pages/user/profile/index.vue
@@ -1,43 +1,14 @@
 <template>
   <div id="page-profile" class="container">
     <h1>User profile</h1>
-    <p>
-      <span class="font-weight-bold">{{ $t('User_name') }}:</span>
-      <span>{{ user.userName }}</span>
-      <b-icon-pencil-square
-        @click="edit"
-        :animation="animation"
-        variant="primary"
-        class="ml-2 icon wc-cursor-pointer"
-        scale="1.2"
-      />
-    </p>
-
-    <div class="row">
-      <b-collapse id="user-name-collapse" class="col-12">
-        <WcForm ref="wcForm" class="px-2">
-          <WcInput
-            id="user-name"
-            :value="userForm.userName"
-            :validation="$v.userForm.userName"
-            :label="$t('User_name') + ':'"
-            :placeholder="$t('User_name')"
-            :state="$v.userForm.userName.$error ? false : null"
-            @input="userForm.userName = $event"
-            type="text"
-            class="w-100"
-          />
-          <b-button @click="closeEdit" variant="secondary">{{
-            $t('Cancel')
-          }}</b-button>
-          <WcButtonSubmit
-            @click.native="submit"
-            :isProcessing="isFormProcessing"
-          />
-        </WcForm>
-      </b-collapse>
-    </div>
-
+    <WcEditOneField
+      :actualValue="user.userName"
+      :wcInputValue="userForm.userName"
+      :validation="$v.userForm.userName"
+      @input="userForm.userName = $event"
+      :endPointPath="endPointPath"
+      :objectToSendServer="objectToSendServer"
+    />
     <p>
       <span class="font-weight-bold">{{ $t('Name') }}:</span>
       <span>{{ user.name }}</span>
@@ -54,24 +25,23 @@
 </template>
 
 <script>
-import { BIconPencilSquare } from 'bootstrap-vue'
 import { requiredIf, email } from 'vuelidate/lib/validators'
 import wcHandleError from '@/mixins/wc-handle-error.js'
-import WcInput from '@/components/forms/WcInput.vue'
-import WcForm from '@/components/forms/WcForm.vue'
-import WcButtonSubmit from '@/components/forms/WcButtonSubmit'
+import WcEditOneField from '@/components/forms/WcEditOneField.vue'
 export default {
   components: {
-    BIconPencilSquare,
-    WcInput,
-    WcForm,
-    WcButtonSubmit
+    WcEditOneField
   },
   mixins: [wcHandleError],
   data() {
     return {
-      isFormProcessing: false,
-      animation: null,
+      endPointPath: '',
+      objectToSendServer: {
+        userId: '',
+        contentToUpdate: {
+          userName: ''
+        }
+      },
       userForm: {
         name: '',
         lastName: '',
@@ -128,48 +98,10 @@ export default {
       this.user.name = userDataGotFromServe.name
       this.user.lastName = userDataGotFromServe.lastName
       this.user.email = userDataGotFromServe.email
+      this.endPointPath = `/user/update/${this.user.id}`
+      this.objectToSendServer.userId = this.user.id
     } catch (error) {
       this.serverErrorsHandler(error)
-    }
-  },
-  methods: {
-    closeEdit() {
-      this.$root.$emit('bv::toggle::collapse', 'user-name-collapse')
-    },
-    edit(field) {
-      this.$root.$emit('bv::toggle::collapse', 'user-name-collapse')
-      this.animation = 'throb'
-      setTimeout(() => {
-        this.animation = null
-      }, 800)
-    },
-    async submit() {
-      try {
-        this.isFormProcessing = true
-        await this.$refs.wcForm.validationForm(this.$v)
-        const objectToSendServer = {
-          userId: this.user.id,
-          contentToUpdate: {}
-        }
-        for (const key in this.userForm) {
-          if (this.userForm[key]) {
-            objectToSendServer.contentToUpdate[key] = this.userForm[key]
-          }
-        }
-        await this.$axios.$post(
-          `/user/update/${this.user.id}`,
-          objectToSendServer,
-          {
-            headers: {
-              Authorization: 'Bearer ' + localStorage.getItem('jwtToken')
-            }
-          }
-        )
-      } catch (error) {
-        this.serverErrorsHandler(error)
-      } finally {
-        this.isFormProcessing = false
-      }
     }
   }
 }
