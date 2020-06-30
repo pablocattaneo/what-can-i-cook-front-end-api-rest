ec659bc379efb981ff3fd6878330ebad2443ed64
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-06-30 16:56:51 -0300

    #master add app description

--- pages/index.vue
+++ pages/index.vue
@@ -14,11 +14,40 @@
     </div>
     <p class="mt-4">{{ $t('home.introduction') }}</p>
     <h2>Techonologies apply</h2>
+    <h3>Front End</h3>
+    <ul>
+      <li>
+        First Server Side Rendering using:
+        <a href="https://nuxtjs.org/" target="_blank">Nuxt</a>
+      </li>
+      <li>
+        <a href="https://bootstrap-vue.org/" target="_blank">BootstrapVue</a>
+      </li>
+      <li>
+        code formatter
+        <a href="https://prettier.io/" target="_blank">Prettier</a>
+      </li>
+    </ul>
+    <h3>Backend End</h3>
+    <ul>
+      <li>Node</li>
+      <li>Express</li>
+      <li>MongoDB</li>
+    </ul>
     <h2>Features</h2>
     <ul>
       <li>
+        Locale to English and Spanish
+      </li>
+      <li>
+        Authentication via JWT
+      </li>
+      <li>
         Inline edit fields:
         <nuxt-link to="/user/profile">User profile</nuxt-link>
+      </li>
+      <li>
+        Handle Erros
       </li>
     </ul>
   </div>
