3e714d9d499ecd73dc6afc28224fc2318cde6cb8
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-06-30 11:50:23 -0300

    #master delete unnecessary code

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,17 @@
 <template>
   <div class="container">
-    <div class="container">
-      <h1 class="Content__Title">
-        {{ $t('home.title') }}
-      </h1>
-      <div class="row">
-        <div class="col-12 d-flex justify-content-center">
-          <b-avatar
-            src="@/assets/img/pablo-cattaneo.jpg"
-            size="175px"
-            alt="Pablo Cattaneo"
-          />
-        </div>
+    <h1 class="Content__Title">
+      {{ $t('home.title') }}
+    </h1>
+    <div class="row">
+      <div class="col-12 d-flex justify-content-center">
+        <b-avatar
+          src="@/assets/img/pablo-cattaneo.jpg"
+          size="175px"
+          alt="Pablo Cattaneo"
+        />
       </div>
-      <p class="mt-4">{{ $t('home.introduction') }}</p>
     </div>
+    <p class="mt-4">{{ $t('home.introduction') }}</p>
   </div>
 </template>
