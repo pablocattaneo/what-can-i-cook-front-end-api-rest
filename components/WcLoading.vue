826ca9ac6b09d828e43891909f3e03a5019d4e2f
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-07-01 18:35:17 -0300

    #master center horizontally and vertically loading

--- components/WcLoading.vue
+++ components/WcLoading.vue
@@ -7,3 +7,24 @@
     />
   </div>
 </template>
+
+<style lang="stylus" scoped>
+#wc-loading
+  position absolute
+  left 0
+  right 0
+  top 0
+  bottom 0
+  margin auto
+  z-index 10
+  background white
+  .wc-loading-spinner
+    position absolute
+    display block
+    left 0
+    right 0
+    top 0
+    bottom 0
+    margin auto
+    z-index 10
+</style>
