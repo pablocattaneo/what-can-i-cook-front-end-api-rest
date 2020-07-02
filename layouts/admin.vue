1025688144cf06af5fbb1cf9905635ab92311e12
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-07-01 22:41:14 -0300

    #master delete unnecessary files

--- layouts/admin.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <nuxt-link :to="switchLocalePath('en')">English</nuxt-link>
-    <nuxt-link :to="switchLocalePath('es')">Español</nuxt-link>
-    <nuxt />
-  </div>
-</template>
