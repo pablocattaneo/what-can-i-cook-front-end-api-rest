f3383bfec69209c19f94a42fa38c22e27afbba4e
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-07-09 00:09:36 -0300

    #master add recipe detail page

---
+++ pages/recipe/_slug.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="recipe-page" class="container">
+    <h1>Recipe</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  async fetch() {
+    try {
+      this.recipes = await this.$axios.$get(
+        `/recipe-by-slug/${this.$route.params.slug}`
+      )
+    } catch (error) {
+      throw error
+    }
+  }
+}
+</script>
