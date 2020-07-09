6561128da731ab297a11faa46ff328f92c5fa56d
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-07-09 00:35:37 -0300

    #master add boostraps row cols and replace img html by b-img and make response img and center it

--- pages/recipe/_slug.vue
+++ pages/recipe/_slug.vue
@@ -1,11 +1,20 @@
 <template>
   <div id="recipe-page" class="container">
-    <h1>{{ recipe.title }}</h1>
-    <img
-      v-if="recipe.mainImg"
-      :src="$store.state.apiRestBaseUrl + recipe.mainImg"
-      :alt="recipe.title"
-    />
+    <div class="row">
+      <div class="col-12">
+        <h1>{{ recipe.title }}</h1>
+      </div>
+    </div>
+    <div v-if="recipe.mainImg" class="row">
+      <div class="col-12">
+        <b-img
+          :src="$store.state.apiRestBaseUrl + recipe.mainImg"
+          :alt="recipe.title"
+          fluid
+          center
+        />
+      </div>
+    </div>
   </div>
 </template>
 
