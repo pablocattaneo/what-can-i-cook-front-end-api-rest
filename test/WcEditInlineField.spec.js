3c1a00502ebc110371388d62b9ade91b17245d5f
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-07-13 10:43:25 -0300

    #master add test for input event

--- test/WcEditInlineField.spec.js
+++ test/WcEditInlineField.spec.js
@@ -1,6 +1,7 @@
 import { mount, createLocalVue } from '@vue/test-utils'
 import WcEditInlineField from '@/components/forms/WcEditInlineField'
 import WcButtonSubmit from '@/components/forms/WcButtonSubmit'
+import WcInput from '@/components/forms/WcInput'
 import Vuex from 'vuex'
 import { BootstrapVue } from 'bootstrap-vue'
 
@@ -48,5 +49,19 @@ describe('WcButtonSubmit click', () => {
     wrapper.vm.submit = jest.fn()
     await wrapper.findComponent(WcButtonSubmit).trigger('click')
     expect(wrapper.vm.submit).toHaveBeenCalled()
+  })
+})
+
+describe('WcInput', () => {
+  test('WcInput must exist', () => {
+    expect(wrapper.findComponent(WcInput).exists()).toBe(true)
+  })
+  test('When user input a value in WcInput value data must store this value', async () => {
+    wrapper.vm.submit = jest.fn()
+    await wrapper
+      .findComponent(WcInput)
+      .find('input[type="text"]')
+      .setValue('An x value')
+    expect(wrapper.vm.value).toBe('An x value')
   })
 })
