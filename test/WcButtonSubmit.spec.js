9e66b6280238695ba0bed84ba773ef4030bfe6ac
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-07-13 23:29:32 -0300

    #master add WcButtonSubmit test and verify if his props are defined

---
+++ test/WcButtonSubmit.spec.js
@@ -0,0 +1,40 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import WcButtonSubmit from '@/components/forms/WcButtonSubmit'
+import { BootstrapVue } from 'bootstrap-vue'
+
+const localVue = createLocalVue()
+localVue.use(BootstrapVue)
+
+let wrapper
+beforeAll(() => {
+  wrapper = shallowMount(WcButtonSubmit, {
+    localVue,
+    mocks: {
+      $t: () => {}
+    }
+  })
+})
+
+describe('Props isProcessing', () => {
+  test('The component is well formed so prop isProcessing should exist', () => {
+    expect(wrapper.vm).toHaveProperty('isProcessing')
+  })
+})
+
+describe('Props disabled', () => {
+  test('The component is well formed so prop disabled should exist', () => {
+    expect(wrapper.vm).toHaveProperty('disabled')
+  })
+})
+
+describe('Props buttonTextValue', () => {
+  test('The component is well formed so prop buttonTextValue should exist', () => {
+    expect(wrapper.vm).toHaveProperty('buttonTextValue')
+  })
+})
+
+describe('Props processingTextValue', () => {
+  test('The component is well formed so prop processingTextValue should exist', () => {
+    expect(wrapper.vm).toHaveProperty('processingTextValue')
+  })
+})
