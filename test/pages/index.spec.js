f34cb1ab0a9399afa10376cf73b30174b772b8cf
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-07-14 20:19:56 -0300

    #master create test for index page

---
+++ test/pages/index.spec.js
@@ -0,0 +1,25 @@
+import { mount, createLocalVue, RouterLinkStub} from '@vue/test-utils'
+import Index from '@/pages/index.vue'
+import { BootstrapVue } from 'bootstrap-vue'
+
+const localVue = createLocalVue()
+localVue.use(BootstrapVue)
+
+let wrapper
+beforeAll(() => {
+  wrapper = mount(Index, {
+    localVue,
+    mocks: {
+      $t: () => {}
+    },
+    stubs: {
+      NuxtLink: RouterLinkStub
+    }
+  })
+})
+
+describe('Componets markup', () => {
+  test('The component is well formed contain css class container', () => {
+    expect(wrapper.classes()).toContain('container')
+  })
+})
