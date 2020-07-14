54251f4785a661613ffce94f47b10ea857e90f10
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-07-13 22:48:21 -0300

    #master create wcHeader test and verify that signOut method is defined

---
+++ test/WcHeader.spec.js
@@ -0,0 +1,36 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import WcHeader from '@/components/WcHeader'
+import { BootstrapVue } from 'bootstrap-vue'
+import Vuex from 'vuex'
+
+const localVue = createLocalVue()
+localVue.use(BootstrapVue)
+localVue.use(Vuex)
+
+const store = new Vuex.Store({
+  modules: {
+    user: {
+      namespaced: true,
+      state: {}
+    }
+  }
+})
+
+let wrapper
+beforeAll(() => {
+  wrapper = shallowMount(WcHeader, {
+    store,
+    localVue,
+    mocks: {
+      localePath: () => {},
+      switchLocalePath: () => {},
+      $t: () => {},
+    }
+  })
+})
+
+describe('signOut method', () => {
+  test('signOut method should be definded', () => {
+    expect(wrapper.vm.signOut).toBeDefined()
+  })
+})
