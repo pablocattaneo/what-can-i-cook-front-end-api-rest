b1d05cb0acb4e71ab662a2f2fdc55dd993a98e0c
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-07-13 23:21:22 -0300

    #master verify that all props of component are defined

---
+++ test/RecipeCard.spec.js
@@ -0,0 +1,65 @@
+import { shallowMount, createLocalVue } from '@vue/test-utils'
+import RecipeCard from '@/components/recipes/RecipeCard'
+import { BootstrapVue } from 'bootstrap-vue'
+
+const localVue = createLocalVue()
+localVue.use(BootstrapVue)
+
+let wrapper
+beforeAll(() => {
+  wrapper = shallowMount(RecipeCard, {
+    localVue,
+    propsData: {
+      readMoreLink: '/',
+      bImgLazySrc: '/'
+    }
+  })
+})
+
+describe('deleteRecipe method', () => {
+  test('deleteRecipe method should be definded', () => {
+    expect(wrapper.vm.deleteRecipe).toBeDefined()
+  })
+})
+
+describe('Props id', () => {
+  test('The component is well formed so prop id should exist', () => {
+    expect(wrapper.vm).toHaveProperty('id')
+  })
+})
+
+describe('Props title', () => {
+  test('The component is well formed so prop title should exist', () => {
+    expect(wrapper.vm).toHaveProperty('title')
+  })
+})
+
+describe('Props bImgLazySrc', () => {
+  test('The component is well formed so prop bImgLazySrc should exist', () => {
+    expect(wrapper.vm).toHaveProperty('bImgLazySrc')
+  })
+})
+
+describe('Props description', () => {
+  test('The component is well formed so prop description should exist', () => {
+    expect(wrapper.vm).toHaveProperty('description')
+  })
+})
+
+describe('Props moreInfo', () => {
+  test('The component is well formed so prop moreInfo should exist', () => {
+    expect(wrapper.vm).toHaveProperty('moreInfo')
+  })
+})
+
+describe('Props borderVariant', () => {
+  test('The component is well formed so prop borderVariant should exist', () => {
+    expect(wrapper.vm).toHaveProperty('borderVariant')
+  })
+})
+
+describe('Props showCardHeader', () => {
+  test('The component is well formed so prop showCardHeader should exist', () => {
+    expect(wrapper.vm).toHaveProperty('showCardHeader')
+  })
+})
