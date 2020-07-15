0f309c9dc15bc6fd77a8c6b62c78fb28e8870984
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-07-15 08:18:00 -0300

    #master add some styles

--- components/WcFooter.vue
+++ components/WcFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="wc-footer" class="mt-5">
+  <div id="wc-footer" class="mt-5 bg-dark">
     <div>
       <ul class="list-unstyled">
         <b-media tag="li">
@@ -69,4 +69,7 @@ export default {
 }
 </script>
 
-<style></style>
+<style lang="stylus" scoped>
+#wc-footer
+  color white
+</style>
