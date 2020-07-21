7444077db528457b8761155b90b81aa60889e4c7
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-07-21 08:20:08 -0300

    #master add a very first version functionality to search recipes

--- components/WcHeader.vue
+++ components/WcHeader.vue
@@ -17,13 +17,18 @@
 
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
-          <b-nav-form>
+          <b-nav-form @submit.stop.prevent>
             <b-form-input
+              v-model="search"
               size="sm"
               class="mr-sm-2"
               placeholder="Search"
             ></b-form-input>
-            <b-button size="sm" class="my-2 my-sm-0" type="submit"
+            <b-button
+              @click="searchMethod"
+              size="sm"
+              class="my-2 my-sm-0"
+              type="submit"
               >Search</b-button
             >
           </b-nav-form>
@@ -62,10 +67,19 @@
 import { mapState } from 'vuex'
 
 export default {
+  data() {
+    return {
+      search: ''
+    }
+  },
   computed: {
     ...mapState('user', ['isUserLogged'])
   },
   methods: {
+    async searchMethod() {
+      const recipes = await this.$axios.$get('recipess/')
+      console.log('recipes', recipes)
+    },
     async signOut() {
       await this.$store.dispatch('user/userSignOutAction')
       this.$router.push(this.localePath('/login'))
