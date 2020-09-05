f538b3d753ffba3f46978a15aa6d9ff87b76ad0d
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-09-05 17:29:51 -0300

    #master improve markup home

--- pages/index.vue
+++ pages/index.vue
@@ -12,50 +12,69 @@
         />
       </div>
     </div>
-    <p class="mt-4">{{ $t('home.introduction') }}</p>
-    <h2>{{ $t('home.applied_technologies') }}</h2>
-    <h3>Front End</h3>
-    <ul>
-      <li>
-        First Server Side Rendering using:
-        <a href="https://nuxtjs.org/" target="_blank">Nuxt</a>
-      </li>
-      <li>
-        <a href="https://bootstrap-vue.org/" target="_blank">BootstrapVue</a>
-      </li>
-      <li>
-        Unit Test {{ $t('through') }}
-        <a href="https://jestjs.io/" target="_blank">Jest</a> {{ $t('and') }}
-        <a href="https://vue-test-utils.vuejs.org/" target="_blank"
-          >Vue Test Utils</a
-        >
-      </li>
-      <li>
-        Code formatter
-        <a href="https://prettier.io/" target="_blank">Prettier</a>
-      </li>
-      <li>
-        Use Stylus as css preprocesor
-        <a href="https://stylus-lang.com/" target="_blank">Stylus</a>
-      </li>
-      <li>
-        Use ES6 features:
-        <a
-          href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining"
-          >Optional chaining</a
-        >
-        <a
-          href="https://github.com/pablocattaneo/what-can-i-cook-front-end-api-rest/blob/master/mixins/wc-handle-error.js"
-          >For example</a
-        >
-      </li>
-    </ul>
-    <h3>Backend End</h3>
-    <ul>
-      <li><a href="https://nodejs.org/en/" target="_blank">Node</a></li>
-      <li><a href="https://expressjs.com/" target="_blank">Express</a></li>
-      <li><a href="https://www.mongodb.com/" target="_blank">MongoDB</a></li>
-    </ul>
+    <div class="row">
+      <div class="col-12">
+        <p class="mt-4">{{ $t('home.introduction') }}</p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">
+        <h2>{{ $t('home.applied_technologies') }}</h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12 col-sm-6">
+        <h3>Front End</h3>
+        <ul>
+          <li>
+            First Server Side Rendering using:
+            <a href="https://nuxtjs.org/" target="_blank">Nuxt</a>
+          </li>
+          <li>
+            <a href="https://bootstrap-vue.org/" target="_blank"
+              >BootstrapVue</a
+            >
+          </li>
+          <li>
+            Unit Test {{ $t('through') }}
+            <a href="https://jestjs.io/" target="_blank">Jest</a>
+            {{ $t('and') }}
+            <a href="https://vue-test-utils.vuejs.org/" target="_blank"
+              >Vue Test Utils</a
+            >
+          </li>
+          <li>
+            Code formatter
+            <a href="https://prettier.io/" target="_blank">Prettier</a>
+          </li>
+          <li>
+            Use Stylus as css preprocesor
+            <a href="https://stylus-lang.com/" target="_blank">Stylus</a>
+          </li>
+          <li>
+            Use ES6 features:
+            <a
+              href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining"
+              >Optional chaining</a
+            >
+            <a
+              href="https://github.com/pablocattaneo/what-can-i-cook-front-end-api-rest/blob/master/mixins/wc-handle-error.js"
+              >For example</a
+            >
+          </li>
+        </ul>
+      </div>
+      <div class="col-12 col-sm-6">
+        <h3>Backend End</h3>
+        <ul>
+          <li><a href="https://nodejs.org/en/" target="_blank">Node</a></li>
+          <li><a href="https://expressjs.com/" target="_blank">Express</a></li>
+          <li>
+            <a href="https://www.mongodb.com/" target="_blank">MongoDB</a>
+          </li>
+        </ul>
+      </div>
+    </div>
     <h2>Site features:</h2>
     <ul>
       <li>
