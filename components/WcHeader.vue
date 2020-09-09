1d2cd736f8bbafba0049508fac1f724780906274
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-09-09 07:11:43 -0300

    #master remove unnecessary html tag

--- components/WcHeader.vue
+++ components/WcHeader.vue
@@ -1,79 +1,77 @@
 <template>
-  <div id="wc-header">
-    <b-navbar toggleable="lg" type="dark" variant="primary">
-      <b-navbar-brand :to="localePath({ path: '/' })">
-        <b-img-lazy
-          :blank-src="null"
-          src="~/assets/img/logo.svg"
-          alt="site logo"
-          title="site logo"
-          class="logo-img"
-        />
-      </b-navbar-brand>
+  <b-navbar id="wc-header" toggleable="lg" type="dark" variant="primary">
+    <b-navbar-brand :to="localePath({ path: '/' })">
+      <b-img-lazy
+        :blank-src="null"
+        src="~/assets/img/logo.svg"
+        alt="site logo"
+        title="site logo"
+        class="logo-img"
+      />
+    </b-navbar-brand>
 
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-navbar-toggle target="nav-collapse" />
 
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item :to="localePath({ path: '/recipes' })">{{
-            $t('links.recipes')
-          }}</b-nav-item>
-        </b-navbar-nav>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item :to="localePath({ path: '/recipes' })">{{
+          $t('links.recipes')
+        }}</b-nav-item>
+      </b-navbar-nav>
 
-        <b-navbar-nav class="ml-auto">
-          <b-nav-form @submit.stop.prevent>
-            <b-form-input
-              v-model="term"
-              :placeholder="$t('Search')"
-              size="sm"
-              class="mr-sm-2"
-            />
-            <b-button
-              @click="searchMethod"
-              size="sm"
-              class="my-2 my-sm-0"
-              type="submit"
-            >
-              <BIconSearch class="mr-1" />
-              {{ $t('Search') }}
-            </b-button>
-          </b-nav-form>
-
-          <b-nav-item-dropdown text="Lang" right>
-            <b-dropdown-item :to="switchLocalePath('en')"> EN</b-dropdown-item>
-            <b-dropdown-item :to="switchLocalePath('es')"> ES</b-dropdown-item>
-          </b-nav-item-dropdown>
-
-          <b-nav-item-dropdown right>
-            <template v-slot:button-content>
-              <em>{{ $t('links.user') }}</em>
-            </template>
-            <b-dropdown-item
-              v-if="isUserLogged"
-              :to="localePath({ path: '/user/profile' })"
-            >
-              <BIconPersonCircle class="mr-1" />
-              {{ $t('links.profile') }}</b-dropdown-item
-            >
-            <b-dropdown-item
-              v-if="!isUserLogged"
-              :to="localePath({ path: '/login' })"
-            >
-              <BIconBoxArrowInRight class="mr-1" />
-              Login</b-dropdown-item
-            >
-            <b-dropdown-item v-if="isUserLogged" @click="signOut">
-              <BIconBoxArrowRight class="mr-1" />
-              {{ $t('links.sign_out') }}</b-dropdown-item
-            >
-          </b-nav-item-dropdown>
-          <b-nav-item :to="localePath({ path: '/signup' })">
-            {{ $t('links.sign_up') }}</b-nav-item
+      <b-navbar-nav class="ml-auto">
+        <b-nav-form @submit.stop.prevent>
+          <b-form-input
+            v-model="term"
+            :placeholder="$t('Search')"
+            size="sm"
+            class="mr-sm-2"
+          />
+          <b-button
+            @click="searchMethod"
+            size="sm"
+            class="my-2 my-sm-0"
+            type="submit"
           >
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-  </div>
+            <BIconSearch class="mr-1" />
+            {{ $t('Search') }}
+          </b-button>
+        </b-nav-form>
+
+        <b-nav-item-dropdown text="Lang" right>
+          <b-dropdown-item :to="switchLocalePath('en')"> EN</b-dropdown-item>
+          <b-dropdown-item :to="switchLocalePath('es')"> ES</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown right>
+          <template v-slot:button-content>
+            <em>{{ $t('links.user') }}</em>
+          </template>
+          <b-dropdown-item
+            v-if="isUserLogged"
+            :to="localePath({ path: '/user/profile' })"
+          >
+            <BIconPersonCircle class="mr-1" />
+            {{ $t('links.profile') }}</b-dropdown-item
+          >
+          <b-dropdown-item
+            v-if="!isUserLogged"
+            :to="localePath({ path: '/login' })"
+          >
+            <BIconBoxArrowInRight class="mr-1" />
+            Login</b-dropdown-item
+          >
+          <b-dropdown-item v-if="isUserLogged" @click="signOut">
+            <BIconBoxArrowRight class="mr-1" />
+            {{ $t('links.sign_out') }}</b-dropdown-item
+          >
+        </b-nav-item-dropdown>
+        <b-nav-item :to="localePath({ path: '/signup' })">
+          {{ $t('links.sign_up') }}</b-nav-item
+        >
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
 </template>
 
 <script>
