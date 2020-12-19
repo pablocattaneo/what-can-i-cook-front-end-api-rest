318f934fbedba345dc1e1c11b2f13fb2812a4059
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-12-19 08:03:46 -0300

    #master it adds mock to fix  Property or method  is not defined on the instance but referenced during render error when run jest

--- test/WcFooter.spec.js
+++ test/WcFooter.spec.js
@@ -8,7 +8,11 @@ localVue.use(BootstrapVue)
 let wrapper
 beforeAll(() => {
   wrapper = mount(WcFooter, {
-    localVue
+    localVue,
+    mocks: {
+      $t: () => {},
+      localePath: () => ''
+    }
   })
 })
 
