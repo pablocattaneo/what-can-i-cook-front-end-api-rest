1092fb5ad54a5c2fa0fd5b64ec8b9b1fe938351f
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-12-24 12:31:10 -0300

    #master It improves the test, reduce the amount of code on it

--- test/pages/index.spec.js
+++ test/pages/index.spec.js
@@ -25,30 +25,6 @@ beforeAll(() => {
               src: '/',
               alt: 'Node'
             }
-          },
-          {
-            html:
-              '<a href="https://expressjs.com/" target="_blank">Express</a>',
-            img: {
-              src: '/',
-              alt: 'Express'
-            }
-          },
-          {
-            html:
-              '<a href="https://www.mongodb.com/" target="_blank">MongoDB</a>',
-            img: {
-              src: '/',
-              alt: 'Mongo DB'
-            }
-          },
-          {
-            html:
-              '<a href="https://www.typescriptlang.org/" target="_blank">Typescript</a>',
-            img: {
-              src: '/',
-              alt: 'Typescript'
-            }
           }
         ],
         frontEndTechnologies: [
@@ -58,50 +34,6 @@ beforeAll(() => {
             img: {
               src: '/',
               alt: 'Nuxt'
-            }
-          },
-          {
-            html:
-              '<a href="https://bootstrap-vue.org/" target="_blank">BootstrapVue</a>',
-            img: {
-              src: '/',
-              alt: 'BootstrapVue'
-            }
-          },
-          {
-            html: `Unit Test ${this.$t(
-              'through'
-            )} <a href="https://jestjs.io/" target="_blank">Jest</a> ${this.$t(
-              'and'
-            )} 
-              <a href="https://vue-test-utils.vuejs.org/" target="_blank">Vue Test Utils</a>`,
-            img: {
-              src: '/',
-              alt: 'Jest'
-            }
-          },
-          {
-            html:
-              'Code formatter <a href="https://prettier.io/" target="_blank">Prettier</a>',
-            img: {
-              src: '/',
-              alt: 'Prettier'
-            }
-          },
-          {
-            html:
-              'Use Stylus as css preprocesor <a href="https://stylus-lang.com/" target="_blank">Stylus</a>',
-            img: {
-              src: '/',
-              alt: 'Stylus'
-            }
-          },
-          {
-            html:
-              'Use ES6 features: <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Optional_chaining">Optional chaining</a> <a href="https://github.com/pablocattaneo/what-can-i-cook-front-end-api-rest/blob/master/mixins/wc-handle-error.js" >For example</a>',
-            img: {
-              src: '/',
-              alt: 'ES6'
             }
           }
         ]
