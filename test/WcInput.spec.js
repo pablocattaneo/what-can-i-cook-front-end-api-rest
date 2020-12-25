f41d80968ba0faddae7afbfb4acb21b7a2bcec2d
Pablo Cattaneo <pablocattaneo1@gmail.com>
2020-12-25 17:18:13 -0300

    #master It adds a comment to explain the reason of line jest.spyOn(console, 'warn').mockImplementation(() => {})

--- test/WcInput.spec.js
+++ test/WcInput.spec.js
@@ -7,6 +7,7 @@ const localVue = createLocalVue()
 let wrapper
 let input
 beforeAll(() => {
+  // It removes boostrap warning in the way a member of Bootrap-vue recommend do it https://github.com/bootstrap-vue/bootstrap-vue/issues/3303\#issuecomment-492718875
   jest.spyOn(console, 'warn').mockImplementation(() => {})
   localVue.use(BootstrapVue)
   wrapper = mount(WcInput, { localVue })
--- test/WcSelect.spec.js
+++ test/WcSelect.spec.js
@@ -6,6 +6,7 @@ const localVue = createLocalVue()
 
 let wrapper
 beforeAll(() => {
+  // It removes boostrap warning in the way a member of Bootrap-vue recommend do it https://github.com/bootstrap-vue/bootstrap-vue/issues/3303\#issuecomment-492718875
   jest.spyOn(console, 'warn').mockImplementation(() => {})
   localVue.use(BootstrapVue)
   wrapper = mount(WcSelect, {
