588144e9c1f4ebf5ee03821919ea32695558ed28
Pablo Cattaneo <pablocattaneo1@gmail.com>
2021-01-11 23:07:10 -0300

    #master it creates user test

--- test/store/user.spec.js
+++ test/store/user.spec.js
@@ -3,3 +3,11 @@ import { mutations } from '~/store/user'
 test('Sanity test', () => {
   expect(true).toBe(true)
 })
+
+describe('Mutations', () => {
+  test('isUserLoggedMutation was called with true as payload so state.isUserLogged should be true ', () => {
+    const state = { isUserLogged: false }
+    mutations.isUserLoggedMutation(state, true)
+    expect(state).toEqual({ isUserLogged: true })
+  })
+})
