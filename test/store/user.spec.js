fcd588abd17570bf6227ae8deb2ba2b28d19a09e
Pablo Cattaneo <pablocattaneo1@gmail.com>
2021-01-20 06:45:14 -0300

    #master it add test jwt state should be set to a valid token

--- test/store/user.spec.js
+++ test/store/user.spec.js
@@ -27,6 +27,25 @@ describe('userLoginAction', () => {
     await flushPromises()
     expect(store.state.isUserLogged).toBe(true)
   })
+  test('should set jwt state to eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1ZjNiYmE4MWYzM2M1MTFhOTA0NTVmYTMiLCJpYXQiOjE2MTEwNTI2ODksImV4cCI6MTYxMTA1NjI4OX0.L9pR2IFbZBPYQWhL4nw47szrjRoVu1ULxj6RgV0Drs8', async () => {
+    const localVue = createLocalVue()
+    localVue.use(Vuex)
+    const storeOtions = { mutations, actions, state: state() }
+    const clonedStoreConfig = cloneDeep(storeOtions)
+    const store = new Vuex.Store(clonedStoreConfig)
+    store.$axios = {
+      $post: jest.fn()
+    }
+    const token =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1ZjNiYmE4MWYzM2M1MTFhOTA0NTVmYTMiLCJpYXQiOjE2MTEwNTI2ODksImV4cCI6MTYxMTA1NjI4OX0.L9pR2IFbZBPYQWhL4nw47szrjRoVu1ULxj6RgV0Drs8'
+    store.$axios.$post.mockReturnValue({
+      token,
+      userId: '5f3bba81f33c511a90455fa3'
+    })
+    store.dispatch('userLoginAction')
+    await flushPromises()
+    expect(store.state.jwt).toBe(token)
+  })
 })
 
 describe('setUserIdState', () => {
