0bdaab67bc362410ea1ca5f4c2e9c104665a256c
Pablo Cattaneo <pablocattaneo1@gmail.com>
2021-01-23 12:12:24 -0300

    #master uses beforeEach intead of beforeAll to mount component in order to avoid leak in the test

--- test/WcButtonSubmit.spec.js
+++ test/WcButtonSubmit.spec.js
@@ -5,7 +5,7 @@ import { BootstrapVue } from 'bootstrap-vue'
 const localVue = createLocalVue()
 
 let wrapper
-beforeAll(() => {
+beforeEach(() => {
   // It removes boostrap warning in the way a member of Bootrap-vue recommend do it https://github.com/bootstrap-vue/bootstrap-vue/issues/3303\#issuecomment-492718875
   jest.spyOn(console, 'warn').mockImplementation(() => {})
   localVue.use(BootstrapVue)
