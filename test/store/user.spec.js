ab1ebfd58a72b399ed8160c7f359b19998c2ed83
Pablo Cattaneo <pablocattaneo1@gmail.com>
2021-01-23 12:06:55 -0300

    #master it moves boilerplate code into beforeEach

--- test/store/user.spec.js
+++ test/store/user.spec.js
@@ -3,20 +3,24 @@ import Vuex from 'vuex'
 import cloneDeep from 'lodash.clonedeep'
 import { mutations, actions, state } from '~/store/user'
 
+let store = {}
+beforeEach(() => {
+  const localVue = createLocalVue()
+  localVue.use(Vuex)
+  const storeOtions = { mutations, actions, state: state() }
+  const clonedStoreConfig = cloneDeep(storeOtions)
+  store = new Vuex.Store(clonedStoreConfig)
+  store.$axios = {
+    $post: jest.fn()
+  }
+})
+
 test('Sanity test', () => {
   expect(true).toBe(true)
 })
 
 describe('userLoginAction', () => {
   test('should set isUserLogged state to true', async () => {
-    const localVue = createLocalVue()
-    localVue.use(Vuex)
-    const storeOtions = { mutations, actions, state: state() }
-    const clonedStoreConfig = cloneDeep(storeOtions)
-    const store = new Vuex.Store(clonedStoreConfig)
-    store.$axios = {
-      $post: jest.fn()
-    }
     store.$axios.$post.mockReturnValue({
       token:
         'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1ZjNiYmE4MWYzM2M1MTFhOTA0NTVmYTMiLCJpYXQiOjE2MTEwNTI2ODksImV4cCI6MTYxMTA1NjI4OX0.L9pR2IFbZBPYQWhL4nw47szrjRoVu1ULxj6RgV0Drs8',
@@ -26,14 +30,6 @@ describe('userLoginAction', () => {
     expect(store.state.isUserLogged).toBe(true)
   })
   test('should set jwt state to eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1ZjNiYmE4MWYzM2M1MTFhOTA0NTVmYTMiLCJpYXQiOjE2MTEwNTI2ODksImV4cCI6MTYxMTA1NjI4OX0.L9pR2IFbZBPYQWhL4nw47szrjRoVu1ULxj6RgV0Drs8', async () => {
-    const localVue = createLocalVue()
-    localVue.use(Vuex)
-    const storeOtions = { mutations, actions, state: state() }
-    const clonedStoreConfig = cloneDeep(storeOtions)
-    const store = new Vuex.Store(clonedStoreConfig)
-    store.$axios = {
-      $post: jest.fn()
-    }
     const token =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1ZjNiYmE4MWYzM2M1MTFhOTA0NTVmYTMiLCJpYXQiOjE2MTEwNTI2ODksImV4cCI6MTYxMTA1NjI4OX0.L9pR2IFbZBPYQWhL4nw47szrjRoVu1ULxj6RgV0Drs8'
     store.$axios.$post.mockReturnValue({
@@ -44,14 +40,6 @@ describe('userLoginAction', () => {
     expect(store.state.jwt).toBe(token)
   })
   test('should set userId state to 5f3bba81f33c511a90455fa3', async () => {
-    const localVue = createLocalVue()
-    localVue.use(Vuex)
-    const storeOtions = { mutations, actions, state: state() }
-    const clonedStoreConfig = cloneDeep(storeOtions)
-    const store = new Vuex.Store(clonedStoreConfig)
-    store.$axios = {
-      $post: jest.fn()
-    }
     const token =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1ZjNiYmE4MWYzM2M1MTFhOTA0NTVmYTMiLCJpYXQiOjE2MTEwNTI2ODksImV4cCI6MTYxMTA1NjI4OX0.L9pR2IFbZBPYQWhL4nw47szrjRoVu1ULxj6RgV0Drs8'
     const userId = '5f3bba81f33c511a90455fa3'
@@ -63,14 +51,6 @@ describe('userLoginAction', () => {
     expect(store.state.userId).toBe(userId)
   })
   test('Should call localStorage.setItem with jwtToken and eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1ZjNiYmE4MWYzM2M1MTFhOTA0NTVmYTMiLCJpYXQiOjE2MTEwNTI2ODksImV4cCI6MTYxMTA1NjI4OX0.L9pR2IFbZBPYQWhL4nw47szrjRoVu1ULxj6RgV0Drs8 as arguments', async () => {
-    const localVue = createLocalVue()
-    localVue.use(Vuex)
-    const storeOtions = { mutations, actions, state: state() }
-    const clonedStoreConfig = cloneDeep(storeOtions)
-    const store = new Vuex.Store(clonedStoreConfig)
-    store.$axios = {
-      $post: jest.fn()
-    }
     const token =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1ZjNiYmE4MWYzM2M1MTFhOTA0NTVmYTMiLCJpYXQiOjE2MTEwNTI2ODksImV4cCI6MTYxMTA1NjI4OX0.L9pR2IFbZBPYQWhL4nw47szrjRoVu1ULxj6RgV0Drs8'
     const userId = '5f3bba81f33c511a90455fa3'
@@ -82,14 +62,6 @@ describe('userLoginAction', () => {
     expect(localStorage.setItem).toHaveBeenCalledWith('jwtToken', token)
   })
   test('The request post request fail so userLoginAction should set isUserLogged state to false', async () => {
-    const localVue = createLocalVue()
-    localVue.use(Vuex)
-    const storeOtions = { mutations, actions, state: state() }
-    const clonedStoreConfig = cloneDeep(storeOtions)
-    const store = new Vuex.Store(clonedStoreConfig)
-    store.$axios = {
-      $post: jest.fn()
-    }
     store.state.isUserLogged = true
     store.$axios.$post.mockRejectedValue('Async error')
     try {
@@ -103,11 +75,6 @@ describe('userLoginAction', () => {
 
 describe('setUserIdState', () => {
   test('localStorage item jwtToken contain a valid jwt so isUserLogged state is set with true value', async () => {
-    const localVue = createLocalVue()
-    localVue.use(Vuex)
-    const storeOtions = { mutations, actions, state: state() }
-    const clonedStoreConfig = cloneDeep(storeOtions)
-    const store = new Vuex.Store(clonedStoreConfig)
     const jwt =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1ZjNiYmE4MWYzM2M1MTFhOTA0NTVmYTMiLCJpYXQiOjE2MTA2MTc4ODksImV4cCI6MTYxMDYyMTQ4OX0.qvLwYk6_ODEF6dAMcdEDNZUQR6cSjPElsZ3fC_fMIqA'
     localStorage.getItem.mockReturnValue(jwt)
@@ -116,22 +83,12 @@ describe('setUserIdState', () => {
   })
 
   test('localStorage item jwtToken contain a falsy value so isUserLogged state is set with false value', async () => {
-    const localVue = createLocalVue()
-    localVue.use(Vuex)
-    const storeOtions = { mutations, actions, state: state() }
-    const clonedStoreConfig = cloneDeep(storeOtions)
-    const store = new Vuex.Store(clonedStoreConfig)
     localStorage.getItem.mockReturnValue(false)
     store.dispatch('setUserIdState')
     expect(store.state.isUserLogged).toBe(false)
   })
 
   test('localStorage item jwtToken contain eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1ZjNiYmE4MWYzM2M1MTFhOTA0NTVmYTMiLCJpYXQiOjE2MTA2MTc4ODksImV4cCI6MTYxMDYyMTQ4OX0.qvLwYk6_ODEF6dAMcdEDNZUQR6cSjPElsZ3fC_fMIqA jwt so userId state is set with "5f3bba81f33c511a90455fa3" value', async () => {
-    const localVue = createLocalVue()
-    localVue.use(Vuex)
-    const storeOtions = { mutations, actions, state: state() }
-    const clonedStoreConfig = cloneDeep(storeOtions)
-    const store = new Vuex.Store(clonedStoreConfig)
     const jwt =
       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1ZjNiYmE4MWYzM2M1MTFhOTA0NTVmYTMiLCJpYXQiOjE2MTA2MTc4ODksImV4cCI6MTYxMDYyMTQ4OX0.qvLwYk6_ODEF6dAMcdEDNZUQR6cSjPElsZ3fC_fMIqA'
     localStorage.getItem.mockReturnValue(jwt)
@@ -142,8 +99,6 @@ describe('setUserIdState', () => {
 
 describe('userSignOutAction', () => {
   test('should set isUserLogged state to false', () => {
-    const localVue = createLocalVue()
-    localVue.use(Vuex)
     const stateWithIsUserLoggedSetToTrue = { ...state(), isUserLogged: true }
     const storeOtions = {
       mutations,
@@ -156,8 +111,6 @@ describe('userSignOutAction', () => {
     expect(store.state.isUserLogged).toBe(false)
   })
   test('should set jwt state to empty string', () => {
-    const localVue = createLocalVue()
-    localVue.use(Vuex)
     const stateWithJwtSetTo123 = { ...state(), jwt: '123' }
     const storeOtions = {
       mutations,
@@ -170,8 +123,6 @@ describe('userSignOutAction', () => {
     expect(store.state.jwt).toBe('')
   })
   test('should set userId state to empty string', () => {
-    const localVue = createLocalVue()
-    localVue.use(Vuex)
     const stateWithUserIdSetToValidId = {
       ...state(),
       userId: '5f3bba81f33c511a90455fa3'
@@ -187,11 +138,6 @@ describe('userSignOutAction', () => {
     expect(store.state.userId).toBe('')
   })
   test('Should call localStorage.removeItem with jwtToken as argument', () => {
-    const localVue = createLocalVue()
-    localVue.use(Vuex)
-    const storeOtions = { mutations, actions, state: state() }
-    const clonedStoreConfig = cloneDeep(storeOtions)
-    const store = new Vuex.Store(clonedStoreConfig)
     store.dispatch('userSignOutAction')
     expect(localStorage.removeItem).toHaveBeenCalledWith('jwtToken')
   })
