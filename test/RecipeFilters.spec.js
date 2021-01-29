30b9f4d4fd3038ab0ad284818594d41f20421889
Pablo Cattaneo <pablocattaneo1@gmail.com>
2021-01-29 08:53:03 -0300

    #master it adds test 'The element #language-0 trigger change event so languages-change event should be trigger with languages as payload'

--- test/RecipeFilters.spec.js
+++ test/RecipeFilters.spec.js
@@ -15,8 +15,16 @@ beforeEach(() => {
     },
     propsData: {
       languages: [
-        { isActive: true, text: 'es', isDisable: true },
-        { isActive: false, text: 'en', isDisable: false }
+        {
+          isActive: true,
+          text: 'es',
+          isDisable: false
+        },
+        {
+          isActive: true,
+          text: 'en',
+          isDisable: false
+        }
       ]
     }
   })
@@ -36,19 +44,24 @@ describe('Props', () => {
   test('The component is well formed so prop languages should exist', () => {
     expect(wrapper.props()).toHaveProperty('languages')
   })
-  test('The props languages was set to [{"isActive":true,"text":"es","isDisable":true},{"isActive":false,"text":"en","isDisable":false}] so #language-0 and #language-1 elements should be render.', async () => {
-    await wrapper.setProps({
-      languages: [
-        { isActive: true, text: 'es', isDisable: true },
-        { isActive: false, text: 'en', isDisable: false }
-      ]
-    })
-    expect(wrapper.findAll('#language-0, #language-1')).toHaveLength(2)
+  test('The props languages was set to [{"isActive":true,"text":"es","isDisable":false},{"isActive":true,"text":"en","isDisable":false}] so #language-0 and #language-1 elements should be render.', () => {
+    expect(wrapper.findAll('input[type="checkbox"]')).toHaveLength(2)
   })
   test('The component is well formed so prop categorySelected should exist', () => {
     expect(wrapper.props()).toHaveProperty('categorySelected')
   })
   test('The component is well formed so prop portionCalories should exist', () => {
     expect(wrapper.props()).toHaveProperty('portionCalories')
+  })
+})
+
+describe('Events', () => {
+  test('The element #language-0 trigger change event so languages-change event should be trigger with languages as payload', async () => {
+    const checkbox = await wrapper.find('#language-0')
+    await checkbox.trigger('change')
+    expect(wrapper.emitted('languages-change')).toHaveLength(1)
+    expect(wrapper.emitted('languages-change')[0]).toEqual([
+      wrapper.vm.languages
+    ])
   })
 })
